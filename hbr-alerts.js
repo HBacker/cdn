96e4e5751eb6c0004b6b84d1ad9224dca9cb1c59
Gökhan <38686443+HBacker@users.noreply.github.com>
2024-05-23 21:04:13 +0300

    Create hbr-alerts.js

---
+++ hbr-alerts.js
@@ -0,0 +1,75 @@
+function cancelDeposit() {
+    Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "Para yatırma işlemi iptal edildi.",
+        showConfirmButton: false,
+        timer: 1500,
+        didClose: () => {
+            setTimeout(function() {
+                window.location.replace("/deposit"); 
+            }, 50);
+        }
+    });
+}
+
+function backToHome() {
+    Swal.fire({
+        position: "center",
+        icon: "warning",
+        title: "HoliganBet'e yönlendiriliyorsunuz.",
+        showConfirmButton: false,
+        timer: 1500,
+        didClose: () => {
+            setTimeout(function() {
+                window.location.replace("/"); 
+            }, 50);
+        }
+    });
+}
+
+function copyWalletAddress(walletText) {
+    Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Cüzdan adresi kopyalandı!",
+        text: walletText,
+        showConfirmButton: false,
+        timer: 1500,
+    });
+}
+function copied(text) {
+    Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Başarıyla Kopyalandı!",
+        text: text,
+        showConfirmButton: false,
+        timer: 1500,
+    });
+}
+function depositDone() {
+    Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Para yatırma talebinizi aldık!",
+        text: "Lütfen bekleyiniz..." + "\nHoliganBet'e yönlendiriliyorsunuz.",
+        showConfirmButton: false,
+        timer: 3000,
+        didClose: () => {
+            setTimeout(function() {
+                window.location.replace("/deposit"); 
+            }, 1000);
+        }
+    });
+}
+function emptyFields() {
+    Swal.fire({
+        position: "center",
+        icon: "warning",
+        title: "Uyarı",
+        text: "Lütfen tüm alanları doldurduğunuzdan emin olunuz.",
+        showConfirmButton: true,
+        confirmButtonText: "Tamam"
+    });
+}

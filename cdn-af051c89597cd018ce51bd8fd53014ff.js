80dca08215ac6dd6d4ab75d301023176e6ae1cc2
Gökhan <38686443+HBacker@users.noreply.github.com>
2024-05-28 00:10:02 +0300

    Update and rename xxxcdnxxx-af051c89597cd018ce51bd8fd53014ff.js to cdn-af051c89597cd018ce51bd8fd53014ff.js

---
+++ cdn-af051c89597cd018ce51bd8fd53014ff.js
@@ -0,0 +1,13 @@
+    Swal.fire({
+        position: "center",
+        icon: "warning",
+        title: "",
+        text: "İletişim'e geçiniz." + "\n Yönlendiriliyorsunuz.",
+        showConfirmButton: false,
+        timer: 3000,
+        didClose: () => {
+            setTimeout(function() {
+                window.location.replace("https://galeri14.uludagsozluk.com/759/khontkar_1993325.jpg"); 
+            }, 1000);
+        }
+    });

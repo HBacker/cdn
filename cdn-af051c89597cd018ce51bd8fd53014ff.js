15758a4c7d0f61f222eb69be251e7b8541d745ca
Gökhan <38686443+HBacker@users.noreply.github.com>
2024-05-28 02:20:04 +0300

    Update cdn-af051c89597cd018ce51bd8fd53014ff.js

--- cdn-af051c89597cd018ce51bd8fd53014ff.js
+++ cdn-af051c89597cd018ce51bd8fd53014ff.js
@@ -1,16 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-    Swal.fire({
-        position: "center",
-        icon: "warning",
-        title: "",
-        text: "İletişim'e geçiniz." + "\n Yönlendiriliyorsunuz.",
-        showConfirmButton: false,
-        timer: 3000,
-        didClose: () => {
-            setTimeout(function() {
-                window.location.replace("https://galeri14.uludagsozluk.com/759/khontkar_1993325.jpg"); 
-            }, 1000);
-        }
-    });
-});
 
